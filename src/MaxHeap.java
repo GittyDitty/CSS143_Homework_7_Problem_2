9159b22520dc0a44cfd57e1b6303e12d0770e0e7
GittyDitty <andrew.d98275@icloud.com>
2020-03-14 12:44:25 -0700

    Final Implementation with tests

--- src/MaxHeap.java
+++ src/MaxHeap.java
@@ -1,35 +1,36 @@
 public class MaxHeap implements Heap {
-    int capacity;
-    int size;
+    int capacity; //amount inside of each which is initialized to zero
+    int size;     //
     Integer[] data;
 
     public MaxHeap(int size) {
         this.size = size;
         data = new Integer[size];
+        capacity = 0;
     }
+
+    // build a heap based on data
+    // to be implemented in O(nlogn)
+    // just use a for loop and add everything into the heap
+    public void MaxHeapLogN(Integer[] data) {
+        // homework
+        if(data.length == 0){
+            System.out.println("Error in assigning max heap of zero");
+        }
+        for(int i=0; i<data.length;i++){
+            add(data[i]);
+        }
+    }
+
     // build a heap based on data
     // to be implemented in O(n)
+    // based on the lecture given we do this by simply putting the data[]
+    // and simply make it into a heap then use maxHeapifyUp to make it into a maxHeap
     public void MaxHeapN(Integer[] data) {
         // homework
-    }
-
-    private int parent(int i){
-        return (i-1)/2;
-    }
-
-    //function that can return the kth child (left or right based on #)
-    private int kthChild(int i, int k){
-        return 2*i + k;
-    }
-
-    //Implemented so that there is no need to type if(size == capacity) over and over again
-    public boolean isFull(){
-        return size == capacity;
-    }
-
-    //Implemented so that there's no need to retype if(size == 0) over and over again
-    private boolean isEmpty(){
-        return size == 0;
+        for(int i=0; i<data.length; i++) {
+            this.data[capacity++] = data[i];
+        }
     }
 
     // add an item to the heap
@@ -38,8 +39,9 @@ public class MaxHeap implements Heap {
         if(isFull()){
             return false;
         }else{
-            data[size++] = item;
-            maxHeapifyUp(size-1);
+            data[capacity] = item;
+            capacity++;
+            maxHeapifyUp(capacity-1);
         }
         return true;
     }
@@ -47,7 +49,7 @@ public class MaxHeap implements Heap {
     // return the max item in the heap
     public Integer get() {
         // homework
-        return findMax();
+        return data[findMax()];
     }
 
     // remove the root item
@@ -56,31 +58,42 @@ public class MaxHeap implements Heap {
         if(isEmpty()){
             return null;
         }
-        int key = data[findMax()];
-        data[findMax()] = data[size-1];
-        size--;
-        maxHeapifyDown(findMax());
+        int key = findMax();
+        data[key] = data[capacity-1];
+        capacity--;
+        maxHeapifyDown(key);
         return key;
     }
 
-    public boolean equals(){
-        //.equals method for comparing heaps which is needed?
+    //Compare this max heap to the contents of the other max heap
+    public boolean equals(MaxHeap other){
+        //.equals method for comparing heaps which is needed
+        if(this.size == other.size && this.capacity == other.capacity){
+            for(int i =0; i< this.size; i++){
+                if(this.data[i] != other.data[i]){
+                    return false;
+                }
+            }
+            return true;
+        }
         return false;
     }
 
+    //Utilized mainly in the add function for scaling from the top to bottom
     private void maxHeapifyUp(int i) {
         int temp = data[i];
-        while(i>0 && temp> data[parent(i)]){
+        while(i > 0 && temp > data[parent(i)]){
             data[i] = data[parent(i)];
             i = parent(i);
         }
         data[i] = temp;
     }
 
+    //Utilized mainly in the pop function for scaling from the bottom to top
     private void maxHeapifyDown(int i){
         int child;
         int temp = data[i];
-        while (kthChild(i,1) < size){
+        while (kthChild(i,1) < capacity){
             child = maxChild(i);
             if(temp < data[child]){
                 data[i] = data[child];
@@ -97,6 +110,7 @@ public class MaxHeap implements Heap {
         int leftChild = kthChild(i,1);
         int rightChild = kthChild(i,2);
 
+        //Good lord I have no idea what I did here but by god if it doesn't work well
         return data[leftChild]>data[rightChild]?leftChild:rightChild;
     }
 
@@ -105,7 +119,30 @@ public class MaxHeap implements Heap {
         if (isEmpty()){
             return 0;
         }
-        return data[0];
+        return 0;//return the root
+    }
+
+    private int parent(int i){
+        if(i%2 == 0){
+            return (i-2)/2;
+        }else{
+            return (i-1)/2;
+        }
+    }
+
+    //function that can return the kth child (left or right based on #)
+    private int kthChild(int i, int k){
+        return 2*i + k;
+    }
+
+    //Implemented so that there is no need to type if(size == capacity) over and over again
+    public boolean isFull(){
+        return size == capacity;
+    }
+
+    //Implemented so that there's no need to retype if(size == 0) over and over again
+    private boolean isEmpty(){
+        return size == 0;
     }
 
 }
--- src/MaxHeapTest.java
+++ src/MaxHeapTest.java
@@ -1,15 +1,53 @@
 import org.junit.Test;
-import java.util.PriorityQueue;
+
+import java.util.concurrent.TimeUnit;
+
 import static org.junit.Assert.*;
 
-@Test
 public class MaxHeapTest {
     // homework
-    MaxHeap test1 = new MaxHeap(0);
-    assertFalse()
+    @Test
+    public void tester() {
+        MaxHeap test1 = new MaxHeap(10);
+        // assertTrue(test1.add(10));
+        Integer[] data = {60, 30, 15, 2, 7, 10, 3};
+        test1.MaxHeapN(data);
+        test1.MaxHeapLogN(data);
+        test1.pop();
+
+        MaxHeap compare = new MaxHeap(10);
+        Integer[] data2 = {30, 15, 2, 7, 10,3};
+        compare.MaxHeapLogN(data2);
+
+        test1.equals(compare);
+        System.out.println(test1);
 
 
-    MaxHeap test2 = new MaxHeap(1);
+        //For the performance comparison tests
+        //testMaxHeapLogN(7);
+        //testMaxHeapN(7);
+    }
+
+    private long testMaxHeapLogN(int size){
+        MaxHeap tester = new MaxHeap(size);
+        Integer[] data = {60,30,15,2,7,10,3};
+        long startTime = System.nanoTime();
+        tester.MaxHeapLogN(data);
+        long endTime = System.nanoTime();
+        System.out.println(TimeUnit.MICROSECONDS.convert(endTime-startTime, TimeUnit.NANOSECONDS)); //for data recording
+        return TimeUnit.MICROSECONDS.convert(endTime-startTime, TimeUnit.NANOSECONDS);
+    }
+
+    private long testMaxHeapN(int size){
+        MaxHeap tester2 = new MaxHeap(size);
+        Integer[] data = {60,30,15,2,7,10,3};
+        long startTime = System.nanoTime();
+        tester2.MaxHeapN(data);
+        long endTime = System.nanoTime();
+        System.out.println(TimeUnit.MICROSECONDS.convert(endTime-startTime, TimeUnit.NANOSECONDS)); //for data recording
+        return TimeUnit.MICROSECONDS.convert(endTime-startTime, TimeUnit.NANOSECONDS);
+    }
+//60,30,15,2,7,10,3
 
 
 }
